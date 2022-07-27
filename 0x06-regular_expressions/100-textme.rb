71058705be25794926daf03050850be293a04c0e
Chukwuma999 <chukwumaobinguma@gmail.com>
2022-07-27 23:46:30 +0200

    Fixing code

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
+# parse logfile and output {sender}, [reciever], [flags]
+
 puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(",")
--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
+# only match capital letters
+
 puts ARGV[0].scan(/[A-Z]/).join
